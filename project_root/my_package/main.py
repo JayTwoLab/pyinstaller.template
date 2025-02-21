565f0e849c1f30e3f463b29cef71d3264b639944
Jay Two <j2doll@gmail.com>
2025-02-21 15:34:09 +0900

    update comments in main.py

--- project_root/my_package/main.py
+++ project_root/my_package/main.py
@@ -3,15 +3,15 @@ import os
 import json
 
 def get_resource_path(relative_path):
-    """ 실행 파일 내부 또는 개발 환경에서 리소스 파일의 경로를 찾음 """
+    """ Locate the path to the resource file inside the executable file or in the development environment """
     if getattr(sys, 'frozen', False):
-        base_path = sys._MEIPASS  # PyInstaller 실행 환경
+        base_path = sys._MEIPASS  # PyInstaller Running Environment
     else:
-        base_path = os.path.dirname(__file__)  # 일반 실행 환경
+        base_path = os.path.dirname(__file__)  # general execution environment
     
     return os.path.join(base_path, relative_path)
 
-# 예제: hello 폴더 내부의 파일 읽기
+# Example: Reading files inside the hello folder
 config_path = get_resource_path("my_package/hello/config.json")
 
 with open(config_path, "r", encoding="utf-8") as f:
